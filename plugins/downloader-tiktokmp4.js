8a9a47e0d55abb67a35b08c15670ac0654b4d769
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-01-18 00:48:42 +0700

    Update downloader-tiktokmp4.js

--- plugins/downloader-tiktokmp4.js
+++ plugins/downloader-tiktokmp4.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('bear', '/tiktok', { url: args[0] } ))).data;
+    let res = await fetch(`https://api.zeeoneofc.xyz/api/downloader/tiktok?url=${args[0]}&APIKEY=2GLx5gnf`)
     if (res.status != 200) throw res.message;
     if (!res) throw res.message;
     

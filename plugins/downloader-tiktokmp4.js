428f4d3a7e49bd9802c9b0b1ca7e76b760f9ac02
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-01-17 23:29:01 +0700

    Update downloader-tiktokmp4.js

--- plugins/downloader-tiktokmp4.js
+++ plugins/downloader-tiktokmp4.js
@@ -1,20 +1,47 @@
-import axios from 'axios'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    
-    let result = `⟐⟞⟚⟝⟮ *Title:* ⟯⟞⟚⟝⟐
-┇⟣⟪ ${res.title} ⟫⟢
-▥ ━┉┄┄┈┈ ▢
+const axios = require('axios')
+const cheerio = require('cheerio')
 
-┇⟐⟞⟚⟝⟮ *Author* ⟯⟞⟚⟝⟐
-▥ ━┉┄┄┈┈ ▢
-${res.author}
-◈ ━┉┈┄┈┈ ►`
-    conn.sendButtonVid(m.chat, res.video, result, '_© Created by aldi_', `Audio`, `.gettt ${args[0]}`, m)
+async function TiktokDownloader (Url) {
+	return new Promise (async (resolve, reject) => {
+		await axios.request({
+			url: "https://ttdownloader.com/",
+			method: "GET",
+			headers: {
+				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+				"accept-language": "en-US,en;q=0.9,id;q=0.8",
+				"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+				"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
+			}
+		}).then(respon => {
+			const $ = cheerio.load(respon.data)
+			const token = $('#token').attr('value')
+			axios({
+				url: "https://ttdownloader.com/req/",
+				method: "POST",
+				data: new URLSearchParams(Object.entries({url: Url, format: "", token: token})),
+				headers: {
+					"accept": "*/*",
+					"accept-language": "en-US,en;q=0.9,id;q=0.8",
+					"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+					"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+					"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
+				}
+			}).then(res => {
+				const ch = cheerio.load(res.data)
+				const result = {
+					status: res.status,
+					result: {
+						nowatermark: ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href'),
+						watermark: ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href'),
+						audio: ch('#results-list > div:nth-child(4)').find(' div.download > a').attr('href')
+					}
+				}
+				resolve(result)
+			}).catch(reject)
+		}).catch(reject)
+	})
 }
+
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktok|ttdl|tt|tiktokdl)$/i

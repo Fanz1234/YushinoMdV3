d53d4cc6f00f5bb66be4fcbb9392b04e762bd2a9
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-01-17 20:56:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -230,7 +230,7 @@ global.APIs = { // API Prefix
   violetics : 'https://violetics.pw',
   ziy : 'https://ziy.herokuapp.com',
   males : 'https://malesin.xyz',
-  bear : 'https://saipulanuar.ga',
+  bear : 'https://api.zeeoneofc.xyz',
   pasha : 'https://api.nataganz.com'
 }
 

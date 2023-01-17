4c2c6d04f95cdcf379305eb9cf02c7d78630d570
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-01-17 19:55:10 +0700

    Update downloader-tiktokmp4.js

--- plugins/downloader-tiktokmp4.js
+++ plugins/downloader-tiktokmp4.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let url = await fetch(`https://saipulanuar.ga/api/download/tiktok?apikey=${bear}&url=${args[0]}`)
-  let json = await url.json()
-    if (url.status !== 200) throw await url.text()
+    let res = (await axios.get(API('bear', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
     
     let result = `⟐⟞⟚⟝⟮ *Title:* ⟯⟞⟚⟝⟐
 ┇⟣⟪ ${res.title} ⟫⟢
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 ▥ ━┉┄┄┈┈ ▢
 ${res.author}
 ◈ ━┉┈┄┈┈ ►`
-    conn.sendButtonVid(m.chat, res.video, result, '_© Created by Fanz', `Audio`, `.gettt ${args[0]}`, m)
+    conn.sendButtonVid(m.chat, res.video, result, '_© Created by aldi_', `Audio`, `.gettt ${args[0]}`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
